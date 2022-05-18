21ccefb4f7bc30501b10cb38ec683227ceff58ea
OrEliyahu43 <eurekas300@gmail.com>
2022-05-18 11:13:30 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import ButtonComponent from './exercises/4.1/ButtonComponent.js';
-import Ex83 from './exercises/17.05.22/Ex8.3.js';
-function App() {
-  return (
-    <Ex83 />
-  );
+import Ex91 from './exercises/17.05.22/Ex9.1.js';
+import React from 'react';
+
+
+export default class App extends React.Component {
+
+  state = {num : 0, showLoading: true}
+
+  componentDidMount(){
+    this.interId = setInterval(() => {
+      this.setState((prev) => {
+        return { num: prev.num + 1 }
+      })
+      console.log(this.state.num)
+    }, 1000)
+
+  }
+
+  componentDidUpdate(){
+    if(this.state.num ===10){
+      clearInterval(this.interId);
+      this.setState((prev) => {
+        return { showLoading : false}
+      })
+
+    }
+  }
+  
+      render(){
+      return (
+          <div>
+
+            {this.state.showLoading && <Ex91 />}
+            
+          </div>
+
+          );
+      }
 };
 
-export default App;
+
 
 
 
---
+++ src/exercises/17.05.22/Ex9.1.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Ex9.1.css'
+
+export default class Ex91 extends React.Component {
+
+    render() {
+        return (
+            <div class="ring">Loading
+                <span></span>
+            </div>
+        )
+    }
+}
+
