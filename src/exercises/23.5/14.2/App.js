c0fbc6e66d8b257e46240cc999eaab687d0d2beb
OrEliyahu43 <eurekas300@gmail.com>
2022-05-23 14:32:26 +0300

    update

---
+++ src/exercises/23.5/14.2/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Ex142 from './exercises/23.5/14.2/Ex14.2';
+  export default class App extends React.Component {
+
+      render(){
+        return(
+
+          <Ex142/>
+        )
+      }
+
+  }
