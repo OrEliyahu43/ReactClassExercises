9935b57cd380bc959c1d3a92422fb1fb186844e5
OrEliyahu43 <eurekas300@gmail.com>
2022-05-24 19:07:51 +0300

    update

---
+++ src/exercises/22.5.22/13.1/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Ex131 from './exercises/22.5.22/13.1/Ex13';
+  export default class App extends React.Component {
+
+      render(){
+        return(
+
+          <Ex131/>
+        )
+      }
+
+  }
